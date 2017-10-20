a52d9e678236f229fde525fb8ca256e6342ccb1b
azurechen <azure517981@gmail.com>
2017-10-20 15:32:27 +0800

    Update the build script.

--- scripts/com.r2studio.MarvelFutureFight2/build.sh
+++ scripts/com.r2studio.MarvelFutureFight2/build.sh
@@ -1,2 +1,1 @@
-zip -r com.r2studio.MarvelFutureFight2.zip .
-adb push ./com.r2studio.MarvelFutureFight2.zip /storage/emulated/0/Robotmon/scripts/
+adb push ./ /storage/emulated/0/Robotmon/scripts/com.r2studio.MarvelFutureFight2
