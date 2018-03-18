c9b3a7669a077583485b4b59bd5bae4ee2c0e01f
poi5305 <poi5305@gmail.com>
2018-03-18 12:04:31 +0800

    recognize some icon

--- scripts/com.r2studio.LineageM/src/main.js
+++ scripts/com.r2studio.LineageM/src/main.js
@@ -20,27 +20,84 @@ class Utils {
     const min = Math.min(Math.min(c.r, c.g), c.b);
     return max - min;
   }
+  static isSameColor(c1, c2, d = 25) {
+    if (Math.abs(c1.r - c2.r) < d && Math.abs(c1.g - c2.g) < d && Math.abs(c1.b - c2.b) < d) {
+      return true;
+    }
+    return false;
+  }
 }
+
 class Rect {
   constructor(ratio, x1, y1, x2, y2) {
+    this._r = ratio;
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
     this.w = x2 - x1;
     this.h = y2 - y1;
-    this._r = ratio;
+    this.tx = this.x1 * this._r;
+    this.ty = this.y1 * this._r;
+    this.tw = (this.x2 - this.x1) * this._r;
+    this.th = (this.y2 - this.y1) * this._r; 
   }
-  get tx() { return this.x1 * this._r; }
-  get ty() { return this.y1 * this._r; }
-  get tw() { return (this.x2 - this.x1) * this._r; }
-  get th() { return (this.y2 - this.y1) * this._r; }
 }
 
 class Point {
-  constructor(x, y) {
+  constructor(ratio, x, y) {
+    this._r = ratio;
     this.x = x;
     this.y = y;
+    this.tx = this.x * this._r;
+    this.ty = this.y * this._r;
+  }
+}
+
+class FeaturePoint extends Point {
+  // need: true => should exist, false => should not exist
+  constructor(ratio, x, y, r, g, b, need, diff = 25) {
+    super(ratio, x, y);
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.d = diff;
+    this.need = need;
+  }
+  check(img) {
+    const c = getImageColor(img, this.tx, this.ty);
+    if (this.need && !Utils.isSameColor(c, this, this.d)) {
+      return false;
+    } else if (!this.need && Utils.isSameColor(c, this)) {
+      return false;
+    }
+    return true;
+  }
+  print(img) {
+    const c = getImageColor(img, this.tx, this.ty);
+    console.log('target', this.tx, this.ty, 'param', `${this._r}, ${this.x}, ${this.y}, ${c.r}, ${c.g}, ${c.b}, true`);
+  }
+}
+
+class PageFeature {
+  constructor(name, featurPoints) {
+    this.name = name || 'Unknown';
+    this.featurPoints = featurPoints || [];
+  }
+  check(img) {
+    for (let i = 0; i < this.featurPoints.length; i++) {
+      const p = this.featurPoints[i];
+      if (!p.check(img)) {
+        return false;
+      }
+      return true;
+    }
+  }
+  print(img) {
+    for (let i = 0; i < this.featurPoints.length; i++) {
+      const p = this.featurPoints[i];
+      p.print(img);
+    }
   }
 }
 
@@ -58,17 +115,64 @@ class GameInfo {
     this.mpBarRect = new Rect(this.ratio, 122, 58, 412, 72);
     this.expBarRect = new Rect(this.ratio, 15, 1069, 1905, 1074);
     this.mapRect = new Rect(this.ratio, 384, 217, 1920, 937); // 1536, 720
+
+    this.menuBtn = new PageFeature('menuOn', [
+      new FeaturePoint(this.ratio, 1844, 56, 245, 245, 241, true),
+      new FeaturePoint(this.ratio, 1844, 66, 128, 70, 56, true),
+      new FeaturePoint(this.ratio, 1844, 76, 245, 220, 215, true),
+    ]);
+    this.autoPlayBtn = new PageFeature('autoPlayOff', [
+      new FeaturePoint(this.ratio, 1429, 767, 140, 154, 127, true, 60),
+      new FeaturePoint(this.ratio, 1476, 772, 140, 157, 130, true, 60),
+    ]);
+    this.selfSkillBtn = new PageFeature('selfSkillOff', [
+      new FeaturePoint(this.ratio, 1594, 601, 141, 147, 137, true, 60),
+      new FeaturePoint(this.ratio, 1591, 624, 117, 128, 114, true, 60),
+    ]);
+    this.attackBtn = new PageFeature('attackOff', [
+      new FeaturePoint(this.ratio, 1634, 769, 165, 180, 170, true, 60),
+    ]);
+  }
+}
+
+class RoleState {
+  constructor(gi) {
+    this.gi = gi;
+    this.hp = 0;
+    this.mp = 0;
+    this.exp = 0;
+    this.currentPage = 'Main';
+    this.isMenuOn = false;
+    this.isSafeRegion = false;
+    this.isAutoPlay = false;
+    this.selfSkill = false;
+    this.leftPanelOn = false;
+    this.rightPanelOn = false;
   }
 }
 
 class LineageM {
   constructor() {
     this.gi = new GameInfo();
+    this.rState = new RoleState(this.gi);
 
     this._img = 0;
 
     this.refreshScreen();
     execute(`mkdir ${getStoragePath}/lineageM`);
+
+    saveImage(this._img, getStoragePath() + '/lineageM/currentScreen.png');
+    for (let i = 0; i < 10; i++) { 
+      this.gi.attackBtn.print(this._img);
+      console.log(this.gi.attackBtn.check(this._img));
+      sleep(200);
+      this.refreshScreen();
+    }
+  }
+
+  // globalState
+  updateGlobalState() {
+    
   }
 
   refreshScreen() {
@@ -119,7 +223,7 @@ class LineageM {
 
   // MAP
   recordCurrentLocation() {
-    const p = new Point(768, 360);
+    const p = new Point(this.gi.ratio, 768, 360);
     const rect1 = new Rect(this.gi.ratio, p.x - 120, p.y - 90, p.x - 30, p.y - 30); // left top
     const rect2 = new Rect(this.gi.ratio, p.x + 30, p.y - 90, p.x + 120, p.y - 30); // right top
     const rect3 = new Rect(this.gi.ratio, p.x - 120, p.y + 30, p.x - 30, p.y + 90); // left bottom
@@ -153,7 +257,7 @@ class LineageM {
   }
 
   findDiffRecordLocation() {
-    const p = new Point(768, 360);
+    const p = new Point(this.gi.ratio, 768, 360);
     const images = [
       openImage(getStoragePath() + '/lineageM/mapRecord1.png'),
       openImage(getStoragePath() + '/lineageM/mapRecord2.png'),
@@ -214,4 +318,4 @@ const lm = new LineageM();
 // const exp = lm.getExpPercent();
 // console.log(hp, mp, exp);
 // lm.recordCurrentLocation();
-lm.getDiffRecordLocation();
+// lm.getDiffRecordLocation();
