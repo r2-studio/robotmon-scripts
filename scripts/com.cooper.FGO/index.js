3d6261c3b915b8ac0aba1b596420eb5e964bd4a9
cooper6334 <cooper6334@gmail.com>
2018-06-07 23:51:19 +0800

    fix chinese unzip failed bug

--- scripts/com.cooper.FGO/index.js
+++ scripts/com.cooper.FGO/index.js
@@ -11,7 +11,7 @@ function loadApi(){
     var apiList = ["basic","start_stage","in_stage","auto_attack_ai","get_box"];    
     var path = getStoragePath();
     for(var i = 0;i<apiList.length;i++){
-        var s = readFile(path+"/scripts/FGO/"+apiList[i]+".js");
+        var s = readFile(path+"/scripts/com.cooper.FGO/"+apiList[i]+".js");
         if(s.length == 0){
             return false;
         }
@@ -35,12 +35,12 @@ function initHTML(){
     execute("mkdir "+path+"/FGO/friend_item");
     if(firstTime.length == 0){
         console.log("First time run script, init basic item");
-        execute("cp "+path+"/scripts/FGO/BasicItem/自動戰鬥.js "+path+"/FGO/script/自動戰鬥.js");
-        execute("cp "+path+"/scripts/FGO/BasicItem/抽箱.js "+path+"/FGO/script/抽箱.js");
-        execute("cp "+path+"/scripts/FGO/BasicItem/友抽.js "+path+"/FGO/script/友抽.js");
-        execute("cp "+path+"/scripts/FGO/BasicItem/孔明.png "+path+"/FGO/friend_servant/孔明.png");
-        execute("cp "+path+"/scripts/FGO/BasicItem/梅林.png "+path+"/FGO/friend_servant/梅林.png");
-        execute("cp "+path+"/scripts/FGO/BasicItem/絆.png "+path+"/FGO/friend_item/絆.png");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/script3.js "+path+"/FGO/script/自動戰鬥.js");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/script2.js "+path+"/FGO/script/抽箱.js");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/script1.js "+path+"/FGO/script/友抽.js");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/friend1.png "+path+"/FGO/friend_servant/孔明.png");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/friend2.png "+path+"/FGO/friend_servant/梅林.png");
+        execute("cp "+path+"/scripts/com.cooper.FGO/BasicItem/item1.png "+path+"/FGO/friend_item/絆.png");
     }
     var scriptList = execute("ls -m "+path+"/FGO/script").replace(/.js/g,'').replace(/ /g,'');
     var servantList = execute("ls -m "+path+"/FGO/friend_servant").replace(/.png/g,'').replace(/ /g,'');
