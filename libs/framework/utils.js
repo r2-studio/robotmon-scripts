092dca920849ce6c94e95104b07eedfd95e49474
poi5305 <poi5305@gmail.com>
2018-06-20 14:39:24 +0800

    add send message in utils

--- libs/framework/utils.js
+++ libs/framework/utils.js
@@ -1,2 +1,35 @@
-
 function Utils() {}
+
+Utils.exist = function(v) {
+  if (typeof v !== 'undefined') {
+    return true;
+  }
+  return false;
+}
+
+Utils.userPlan = -1;
+Utils.lastSendTime = 0;
+
+Utils.canSendMessage = function() {
+  Utils.userPlan = -1;
+  if (Utils.exist(getUserPlan) && Utils.exist(sendNormalMessage)) {
+    Utils.userPlan = getUserPlan();
+  }
+  if (Utils.userPlan === -1) {
+    console.log('Can not send message, should login');
+    return false;
+  }
+  return true;
+}
+
+Utils.sendNormalMessage = function() {
+  if (Utils.canSendMessage()) {
+    console.log(sendNormalMessage(topMsg, msg));
+  }
+}
+
+Utils.sendUrgentMessage = function() {
+  if (Utils.canSendMessage()) {
+    console.log(sendUrgentMessage(topMsg, msg));
+  }
+}
