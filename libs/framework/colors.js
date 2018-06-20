eede0bc9579b27e31d7c959feaaf9cd00ab64eda
poi5305 <poi5305@gmail.com>
2018-06-20 14:50:40 +0800

    calculate color identity score

--- libs/framework/colors.js
+++ libs/framework/colors.js
@@ -24,6 +24,14 @@ Colors.distanceColor = function (c, c1) {
   return Math.sqrt((c1.r - c.r)*(c1.r - c.r) + (c1.g - c.g)*(c1.g - c.g) + (c1.b - c.b)*(c1.b - c.b));
 }
 
+Colors.identityScore = function(e1, e2) {
+  var mean = (e1.r + e2.r) / 2;
+  var r = e1.r - e2.r;
+  var g = e1.g - e2.g;
+  var b = e1.b - e2.b;
+  return Math.sqrt((((512+mean)*r*r)>>8) + 4*g*g + (((767-mean)*b*b)>>8)) / 255;
+}
+
 Colors.minMaxDiff = function (c) {
   const max = Math.max(Math.max(c.r, c.g), c.b);
   const min = Math.min(Math.min(c.r, c.g), c.b);
