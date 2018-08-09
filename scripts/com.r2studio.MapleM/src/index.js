118a7f472de162adcd78adfe2ad48b231d02553d
poi5305 <poi5305@gmail.com>
2018-08-09 11:02:03 +0800

    resolution

--- scripts/com.r2studio.MapleM/src/index.js
+++ scripts/com.r2studio.MapleM/src/index.js
@@ -1,210 +1,102 @@
 // ===== developer global config start =====
 var wh = getScreenSize();
-var gUserScreenWidth = wh.width;
-var gUserScreenHeight = wh.height;
+var gUserScreenWidth = Math.max(wh.width, wh.height);
+var gUserScreenHeight = Math.min(wh.width, wh.height);
 var gDevScreenWidth = 1920;
 var gDevScreenHeight = 1080;
-var gTargetWidth = 800;
-var gTargetHeight = 450;
-var gDevToResizeRatio = 1/2.4;
-var gGameWidth = gUserScreenWidth;
-var gGameHeight = gUserScreenHeight;
+var gResizeWidth = 960; // 960
+var gResizeHeight = 540; // 540
+var gDevToResizeRatio = 1/2;
 var gDevToUserRatio = 1;
-var gGameOffsetX = 0;
+var gGameOffsetX = 0; // for full screen
 var gGameOffsetY = 0;
 var gUserScreenWHType = 0;
 if (gUserScreenWidth / gUserScreenHeight > 1.777778) {
   // more width, align height
   gUserScreenWHType = 1;
-  gDevToUserRatio = gGameHeight / gDevScreenHeight;
-  gGameWidth = Math.round(gGameHeight * 1.777778);
-  gGameOffsetX = (gUserScreenWidth - gGameWidth) / 2;
+  gDevToUserRatio = gUserScreenHeight / gDevScreenHeight;
+  var w = Math.round(gUserScreenHeight * 1.777778);
+  gGameOffsetX = (gUserScreenWidth - w) / 2;
+  gResizeWidth = Math.floor(gUserScreenWidth * gResizeHeight / gUserScreenHeight);
 } else if (gUserScreenWidth / gUserScreenHeight < 1.777778) {
   // less width, align width
   gUserScreenWHType = -1;
-  gDevToUserRatio = gGameWidth / gDevScreenWidth;
-  gGameHeight = Math.round(gGameWidth / 1.777778);
-  gGameOffsetY = (gUserScreenHeight - gGameHeight) / 2;
+  gDevToUserRatio = gUserScreenWidth / gDevScreenWidth;
+  var h = Math.round(gUserScreenWidth / 1.777778);
+  gGameOffsetY = (gUserScreenHeight - h) / 2;
+  gResizeHeight = Math.floor(gUserScreenHeight * gResizeWidth / gUserScreenHeight);
 }
-var gDevToUserRatio = gGameWidth / gDevScreenWidth;
+var gUserToResizeRatio = gResizeHeight / gUserScreenHeight;
+console.log('Resize WH', gResizeWidth, gResizeHeight);
 // ===== developer global config end =====
 
-// ===== init robotmon and basic functions =====
-var robotmon = new Robotmon();
+// ===== Utils =====
+
+var LocLT = 'lt';
+var LocRT = 'rt';
+var LocLB = 'lb';
+var LocRB = 'rb';
+var LocFull = 'full';
+var LocCB = 'cb';
+
+function devToUserXY(xy, loc) {
+  var x = xy.x * gDevToUserRatio;
+  var y = xy.y * gDevToUserRatio;
+  if (loc === LocRT && gUserScreenWHType === 1) {
+    x = gUserScreenWidth - ((gDevScreenWidth - xy.x) * gDevToUserRatio);
+  } else if (loc === LocLB && gUserScreenWHType === -1) {
+    y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
+  } else if (loc === LocRB) {
+    if (gUserScreenWHType === 1) {
+      x = gUserScreenWidth - ((gDevScreenWidth - xy.x) * gDevToUserRatio);
+    } else if (gUserScreenWHType === -1) {
+      y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
+    }
+  } else if (loc === locFull) {
+    x = gGameOffsetX + xy.x * gDevToUserRatio;
+    y = gGameOffsetY + xy.y * gDevToUserRatio;
+  } else if (loc === locCB) {
+    if (gUserScreenWHType === 1) {
+      var w = gDevScreenWidth * gDevToUserRatio;
+      var offsetX = Math.floor((gUserScreenWidth - w) / 2);
+      x = offsetX + (xy.x * gDevToUserRatio);
+    } else if (gUserScreenWHType === -1) {
+      y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
+    }
+  }
+  return xy;
+}
+
+function devToResizeXY(xy, loc) {
+  var userXY = devToUserXY(xy, loc);
+  return {x: userXY.x*gUserToResizeRatio, y: userXY.y*gUserToResizeRatio};
+}
+
+
+// ===== MapleM script =====
+
+function MapleM(config) {
+  this.config = config;
+  this.running = false;
+}
+
+MapleM.prototype.getScreenshot = function() {
+  return getScreenshotModify(0, 0, gResizeWidth, gResizeHeight, gResizeWidth, gResizeHeight, 90);
+}
+
+var mapleM = undefined;
 
 function stop() {
-  robotmon.stop();
-}
-
-function start() {
-
-}
-
-// =====
-
-function devToUserAtLeftTop(xy) {
-  if (gUserScreenWHType == 1) {
-    return {x: xy.x * gDevToUserRatio, y: xy.y * gDevToUserRatio};
-  } else if (gUserScreenWHType == -1) {
-    return {x: xy.x * gDevToUserRatio, y: xy.y * gDevToUserRatio};
+  if (mapleM !== undefined) {
+    mapleM.stop();
+    mapleM = undefined;
   }
-  return xy;
 }
 
-function devToUserAtRightTop(xy) {
-  if (gUserScreenWHType == 1) {
-    var x = gUserScreenWidth - ((gDevScreenWidth - xy.x) * gDevToUserRatio);
-    return {x: x, y: xy.y * gDevToUserRatio};
-  } else if (gUserScreenWHType == -1) {
-    return {x: xy.x * gDevToUserRatio, y: xy.y * gDevToUserRatio};
+function start(configString) {
+  var config = JSON.parse(configString)
+  if (mapleM === undefined) {
+    mapleM = new MapleM(config);
+    mapleM.start();
   }
-  return xy;
 }
-
-function devToUserAtRightBottom(xy) {
-  if (gUserScreenWHType == 1) {
-    var x = gUserScreenWidth - ((gDevScreenWidth - xy.x) * gDevToUserRatio);
-    return {x: x, y: xy.y * gDevToUserRatio};
-  } else if (gUserScreenWHType == -1) {
-    var y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
-    return {x: xy.x * gDevToUserRatio, y: y};
-  }
-  return xy;
-}
-
-function devToUserAtLeftBottom(xy) {
-  if (gUserScreenWHType == 1) {
-    return {x: xy.x * gDevToUserRatio, y: xy.y * gDevToUserRatio};
-  } else if (gUserScreenWHType == -1) {
-    var y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
-    return {x: xy.x * gDevToUserRatio, y: y};
-  }
-  return xy;
-}
-
-function devToUserAtFullScreen(xy) {
-  return {x: gGameOffsetX + xy.x, y: gGameOffsetY + xy.y};
-}
-
-function devToUserAtCenterBottom(xy) {
-  if (gUserScreenWHType == 1) {
-    var w = gDevScreenWidth * gDevToUserRatio;
-    var offsetX = Math.floor((gUserScreenWidth - w) / 2);
-    var x = offsetX + (xy.x * gDevToUserRatio);
-    return {x: x, y: xy.y * gDevToUserRatio};
-  } else if (gUserScreenWHType == -1) {
-    var y = gUserScreenHeight - ((gDevScreenHeight - xy.y) * gDevToUserRatio);
-    return {x: xy.x * gDevToUserRatio, y: y};
-  }
-  return xy;
-}
-
-// ===== game features =====
-
-// ===== game pages =====
-var gamePage = new Page("gamePage");
-gamePage.onPage = function(img) {
-  // var img = this.onScreenshot();
-  // var isFeature = fInGamePage.checkColor();
-  // releaseImage(img);
-  // console.log(isFeature);
-  return true;
-};
-gamePage.onInit = function() {
-};
-gamePage.onEnter = function() {
-  this.context.debug('Enter', this.name);
-};
-gamePage.onExit = function() {
-  this.context.debug('Exit', this.name);
-};
-gamePage.onScreenshot = function() {
-  return getScreenshotModify(0, 0, gGameWidth, gGameHeight, gTargetWidth, gTargetHeight, 90);
-};
-gamePage.onDevToUserXY = function(devX, devY) {
-  var x = devX * gDevToUserRatio;
-  var y = devY * gDevToUserRatio;
-  return {x: x, y: y};
-}
-gamePage.onDevToResizeXY = function(devX, devY) {
-  var x = devX * gDevToResizeRatio;
-  var y = devY * gDevToResizeRatio;
-  return {x: x, y: y};
-}
-
-// ===== game tasks =====
-var goInToGameTask = new Task("gamePage", "goInToGameTask");
-goInToGameTask.onTask = function(img) {
-  return true;
-};
-goInToGameTask.onInit = function() {};
-goInToGameTask.onEnter = function() {
-  this.context.debug('Enter', this.name);
-};
-goInToGameTask.onExit = function() {
-  this.context.debug('Exit', this.name);
-};
-goInToGameTask.onRun = function() {
-  
-};
-
-// var updateStatusTask = new Task("gamePage", "updateStatus");
-// updateStatusTask.onTask = function(img) {
-//   return false;
-// };
-// updateStatusTask.onInit = function() {};
-// updateStatusTask.onEnter = function() {
-//   this.context.debug('Enter', this.name);
-// };
-// updateStatusTask.onExit = function() {
-//   this.context.debug('Exit', this.name);
-// };
-// updateStatusTask.onRun = function() {
-//   // var hp = this.getHP();
-//   // var mp = this.getMP();
-//   // this.expChanged();
-//   // this.isAutoOn();
-//   // console.log('time', hp, mp);
-//   // var region = this.inRegion();
-//   // this.context.debug(region);
-//   this.isMenuOn();
-// };
-
-// ===== developer test utils =====
-function getScreenColorByPoint(p) {
-  var img = gamePage.onScreenshot();
-  var xy = gamePage.onDevToResizeXY(p.x, p.y);
-  var c = getImageColor(img, xy.x, xy.y);
-  var s = Colors.identityScore(p, c).toFixed(3);
-  console.log("new Point("+p.x.toFixed(0)+", "+p.y.toFixed(0)+", "+c.r+", "+c.g+", "+c.b+", true, "+s+");");
-  releaseImage(img);
-}
-
-function getScreenColorByFeature(f) {
-  var img = gamePage.onScreenshot();
-  for (var i = 0; i < f.points.length; i++) {
-    var p = f.points[i];
-    var xy = gamePage.onDevToResizeXY(p.x, p.y);
-    var c = getImageColor(img, xy.x, xy.y);
-    var s = Colors.identityScore(p, c).toFixed(3);
-    console.log("new Point("+p.x.toFixed(0)+", "+p.y.toFixed(0)+", "+c.r+", "+c.g+", "+c.b+", true, "+s+");");
-  }
-  releaseImage(img);
-}
-
-var mapleNews1 = new Point(1740, 96, 254, 255, 248, true, 0.960);
-var mapleNews2 = new Point(1718, 74, 245, 247, 242, true, 0.960);
-var mapleNews3 = new Point(1738, 72, 24, 27, 20, true, 0.960);
-var fMapleNews = new Feature("fMapleNews", [
-  mapleNews1, mapleNews2, mapleNews3
-]);
-
-
-// ===== robotmon register =====
-// robotmon.registAll();
-// robotmon.start();
-
-// getScreenColorByFeature(fMapleNews);
-robotmon.addTask(gamePage, goInToGameTask);
-
-robotmon.start();
