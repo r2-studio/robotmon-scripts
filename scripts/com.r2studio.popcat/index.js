eae0393c5a1c5d2340d3f72044cc142baea88a8f
Andy <poi5305@gmail.com>
2021-08-16 11:13:38 +0800

    add normal mode

--- scripts/com.r2studio.popcat/index.js
+++ scripts/com.r2studio.popcat/index.js
@@ -1,32 +1,40 @@
 var run = false;
 var x = 0;
 var y = 0;
+var normal = true;
 
 function loop() {
-  console.log('Start Clicking...');
+  console.log("Start Clicking...");
   var count = 0;
   while (run) {
-    for (var i = 0; i < 5; i++) {
-      tapDown(x + i * 50, y + i * 50, 1, i);
+    if (normal) {
+      tap(x, y, 20);
+      count++;
+      sleep(160);
+    } else {
+      for (var i = 0; i < 5; i++) {
+        tapDown(x + i * 50, y + i * 50, 1, i);
+      }
+      sleep(5);
+      for (var i = 0; i < 5; i++) {
+        tapUp(x + i * 50, y + i * 50, 1, i);
+      }
+      sleep(5);
+      count++;
     }
-    sleep(5);
-    for (var i = 0; i < 5; i++) {
-      tapUp(x + i * 50, y + i * 50, 1, i);
-    }
-    sleep(5);
-    count++;
     if (count % 50 === 0) {
-      console.log('click', count, '* 5 times');
+      console.log("click", count, "* 5 times");
       sleep(1000);
     }
   }
 }
 
-function start() {
+function start(n) {
   var wh = getScreenSize();
   x = Math.floor(wh.width / 3) || 200;
   y = Math.floor(wh.height / 2) || 200;
   run = true;
+  normal = n;
   loop();
 }
 
