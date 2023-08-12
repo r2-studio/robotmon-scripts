d944e18630fa7126b8a74b53d67ee33d0020f948
ellie <bc70522@gmail.com>
2023-08-12 12:26:39 +0800

    rerouter change command

--- scripts/Rerouter/src/utils.ts
+++ scripts/Rerouter/src/utils.ts
@@ -87,6 +87,22 @@ export class Utils {
       `ANDROID_DATA=/data BOOTCLASSPATH=/system/framework/core-oj.jar:/system/framework/core-libart.jar:/system/framework/conscrypt.jar:/system/framework/okhttp.jar:/system/framework/core-junit.jar:/system/framework/bouncycastle.jar:/system/framework/ext.jar:/system/framework/framework.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/mms-common.jar:/system/framework/android.policy.jar:/system/framework/apache-xml.jar:/system/framework/org.apache.http.legacy.boot.jar am start -n ${packageName}`
     );
     execute(`ANDROID_DATA=/data monkey --pct-syskeys 0 -p ${packageName} -c android.intent.category.LAUNCHER 1`);
+    execute(
+      'ANDROID_BOOTLOGO=1 ' +
+        'ANDROID_ROOT=/system ' +
+        'ANDROID_ASSETS=/system/app ' +
+        'ANDROID_DATA=/data ' +
+        'ANDROID_STORAGE=/storage ' +
+        'ANDROID_ART_ROOT=/apex/com.android.art ' +
+        'ANDROID_I18N_ROOT=/apex/com.android.i18n ' +
+        'ANDROID_TZDATA_ROOT=/apex/com.android.tzdata ' +
+        'EXTERNAL_STORAGE=/sdcard ' +
+        'ASEC_MOUNTPOINT=/mnt/asec ' +
+        'BOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/core-icu4j.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/framework-atb-backward-compatibility.jar:/apex/com.android.conscrypt/javalib/conscrypt.jar:/apex/com.android.media/javalib/updatable-media.jar:/apex/com.android.mediaprovider/javalib/framework-mediaprovider.jar:/apex/com.android.os.statsd/javalib/framework-statsd.jar:/apex/com.android.permission/javalib/framework-permission.jar:/apex/com.android.sdkext/javalib/framework-sdkextensions.jar:/apex/com.android.wifi/javalib/framework-wifi.jar:/apex/com.android.tethering/javalib/framework-tethering.jar ' +
+        'DEX2OATBOOTCLASSPATH=/apex/com.android.art/javalib/core-oj.jar:/apex/com.android.art/javalib/core-libart.jar:/apex/com.android.art/javalib/core-icu4j.jar:/apex/com.android.art/javalib/okhttp.jar:/apex/com.android.art/javalib/bouncycastle.jar:/apex/com.android.art/javalib/apache-xml.jar:/system/framework/framework.jar:/system/framework/ext.jar:/system/framework/telephony-common.jar:/system/framework/voip-common.jar:/system/framework/ims-common.jar:/system/framework/framework-atb-backward-compatibility.jar ' +
+        'SYSTEMSERVERCLASSPATH=/system/framework/com.android.location.provider.jar:/system/framework/services.jar:/system/framework/ethernet-service.jar:/apex/com.android.permission/javalib/service-permission.jar:/apex/com.android.ipsec/javalib/android.net.ipsec.ike.jar ' +
+        `monkey --pct-syskeys 0 -p ${packageName} -c android.intent.category.LAUNCHER 1`
+    );
   }
 
   public static stopApp(packageName: string) {
@@ -120,6 +136,11 @@ export class Utils {
       activityName = result[1].trim();
     }
     return [packageName, activityName];
+  }
+
+  public static isAppOnTop(packageName: string): boolean {
+    const topInfo = execute('dumpsys activity activities | grep mResumedActivity');
+    return topInfo.indexOf(packageName) !== -1;
   }
 
   public static sendSlackMessage(url: string, title: string, message: string) {
