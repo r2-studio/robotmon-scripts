66bb33e3b1e09b7a07034cdca302727b5ab5eb56
ellie <bc70522@gmail.com>
2023-09-09 15:48:00 +0800

    fea: mlb delete old files

--- scripts/com.r2studio.MLB9InningsBasic/src/defaultConfig.ts
+++
@@ -1,8 +0,0 @@
-import { ScriptConfig } from './types';
-import { leagueYearMin } from './constants';
-
-export const defaultConfig: ScriptConfig = {
-  isLocalPaid: false,
-  leagueSeasonMode: 'full',
-  leagueYear: leagueYearMin,
-};
