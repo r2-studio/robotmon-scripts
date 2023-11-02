fdfccb3044068327353883a6075a295a05acb2e8
Marcus Krassmann <marcus.krassmann@gmail.com>
2023-11-02 22:44:00 +0100

    TsumBeta: Add build date to distribution file(s)

--- scripts/com.r2studio.TsumBeta/build.sh
+++ scripts/com.r2studio.TsumBeta/build.sh
@@ -1,3 +1,18 @@
-rm index.zip
-zip index.zip index.js index.html settings.js
-adb push index.zip sdcard/Robotmon/scripts/com.r2studio.TsumBeta/index.zip
+files=( index.js index.html settings.js )
+
+rm -rf dist 2>/dev/null
+mkdir dist
+
+# shellcheck disable=SC2155
+export BUILD_DATE="$(date "+%F %H:%M:%S %:z")" # used by envsubst later
+
+for file in "${files[@]}"; do
+  envsubst \$BUILD_DATE < "$file" > dist/"$file"
+done
+
+(
+cd dist || exit
+zip ../index.zip -- *
+)
+
+#adb push index.zip sdcard/Robotmon/scripts/com.r2studio.TsumBeta/index.zip
