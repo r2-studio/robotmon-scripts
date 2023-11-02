1c6ee8cef1ffeba7371f570ae851cdf3b66a7b59
Marcus Krassmann <marcus.krassmann@gmail.com>
2023-11-02 16:55:55 +0100

    TsumBeta: Extract JS from index.html
    Huge JS within index.html makes further development harder than it needs to be.

--- scripts/com.r2studio.TsumBeta/build.ps1
+++ scripts/com.r2studio.TsumBeta/build.ps1
@@ -1,4 +1,4 @@
-$files = 'index.html', 'index.js'
+$files = 'index.html', 'index.js', 'settings.js'
 foreach ($file in $files)
 {
     adb push $file sdcard/Download/Robotmon/scripts/com.r2studio.TsumBeta/$file
--- scripts/com.r2studio.TsumBeta/build.sh
+++ scripts/com.r2studio.TsumBeta/build.sh
@@ -1,3 +1,3 @@
 rm index.zip
-zip index.zip index.js index.html
+zip index.zip index.js index.html settings.js
 adb push index.zip sdcard/Robotmon/scripts/com.r2studio.TsumBeta/index.zip
