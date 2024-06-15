6da4da0447437aedeb28323d7a59bed4c874ff4d
ellie <bc70522@gmail.com>
2024-06-15 13:18:21 +0800

    mlb/fea: ensure publish only if is production version

--- scripts/com.r2studio.MLB9InningsBasic/build.sh
+++ scripts/com.r2studio.MLB9InningsBasic/build.sh
@@ -1,3 +1,6 @@
+# ensure it's production version
+(grep "exports.isProduction = true" ./index.js) || (echo "error not build bc is not production version" & exit 1)
+
 zip index.zip ./index.js ./index.html
 echo "com.r2studio.MLB9InningsBasic done"
 
