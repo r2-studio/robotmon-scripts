a0ca803fd2a561b019246c842c404bc8f7ecfb67
cooper6334 <cooper6334@gmail.com>
2024-11-29 14:20:09 +0900

    FGOTW V3.66 fix new servant skill bug

--- scripts/com.cooper.FGOTW/index.js
+++ scripts/com.cooper.FGOTW/index.js
@@ -5,7 +5,7 @@ var itemPath;
 var server;
 var loadApiCnt;
 
-var version = "V3.65";
+var version = "V3.66";
 
 function start(loopTime, script, scriptName, be, pref) {
   startScript(loopTime, script, scriptName, be, pref);
