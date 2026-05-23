883363cf6682b8b2163465770229de81ce3810d5
cooper6334 <cooper6334@gmail.com>
2026-05-24 03:10:20 +0900

    FGOTW V3.79 fix gacha and skill ui

--- scripts/com.cooper.FGOTW/index.js
+++ scripts/com.cooper.FGOTW/index.js
@@ -5,8 +5,7 @@ var itemPath;
 var server;
 var loadApiCnt;
 
-
-var version = "V3.78";
+var version = "V3.79";
 
 function start(loopTime, script, scriptName, be, pref) {
   startScript(loopTime, script, scriptName, be, pref);
