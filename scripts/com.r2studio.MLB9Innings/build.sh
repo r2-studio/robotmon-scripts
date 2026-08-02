fef944e8cb5fafb8324b34c78d9ae6aae7b8ee32
Sean@M2 <sean.h.hsueh@gmail.com>
2026-08-03 04:13:08 +1000

    MLB update deploy flow and sync XR

--- scripts/com.r2studio.MLB9Innings/build.sh
+++ scripts/com.r2studio.MLB9Innings/build.sh
@@ -1,11 +1,26 @@
-# ensure it's production version
-IS_PRODUCTION=$(grep "exports.isProduction = true" ./index.js)
-if [ -z "$IS_PRODUCTION" ]; then
-    echo "Please set production version"
-    exit 1
+#!/bin/bash
+set -e
+
+SCRIPT_ID="com.r2studio.MLB9Innings"
+
+grep -q "package: \"${SCRIPT_ID}\"" index.html || {
+  echo "Error: index.html is not premium edition (expected package: ${SCRIPT_ID})"
+  exit 1
+}
+
+grep -q 'isLocalPaid: true' index.html || {
+  echo "Error: index.html missing isLocalPaid: true (premium edition)"
+  exit 1
+}
+
+if grep -q 'exports.isProduction = true' index.js; then
+  echo "Building premium PRODUCTION script"
+else
+  echo "Building premium TEST script"
 fi
 
+rm -f index.zip
 zip index.zip ./index.js ./index.html
-echo "com.r2studio.MLB9Innings done"
+echo "${SCRIPT_ID} done"
 
-# adb push ./index.js ./index.html storage/emulated/0/Download/Robotmon/scripts/com.r2studio.MLB9InningsBasic/
+# adb push ./index.js ./index.html /storage/emulated/0/Download/Robotmon/scripts/com.r2studio.MLB9Innings/
